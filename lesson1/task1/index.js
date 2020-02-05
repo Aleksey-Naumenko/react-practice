3867aed18f6ce238f5f9fa92c61896bcf5cc6b75
Aleksey <aleksey_naumenko@ukr.net>
2020-02-05 18:39:24 +0200

    1

---
+++ lesson1/task1/index.js
@@ -0,0 +1,9 @@
+const rootElement = document.querySelector('#root');
+
+const greetingElem = React.createElement(
+    'div',
+    { className: 'greeting' },
+    'Hello, React!'
+);
+
+ReactDOM.render(greetingElem, rootElement);
