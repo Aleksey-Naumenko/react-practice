7e925382ad927c35defeb34f4082f5630342e6ec
Aleksey <aleksey_naumenko@ukr.net>
2020-02-07 11:55:09 +0200

    hw1

--- lesson3/hw1/src/index.jsx
+++ lesson3/hw1/src/index.jsx
@@ -2,10 +2,10 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import Greeting from './Greeting';
 
-const rootElement = document.querySelector('#greeting');
+const rootElement = document.querySelector('#root');
 
 ReactDOM.render(<Greeting
-    name="John"
+    firstName="John"
     lastName="Doe"
     birthDate={"1992-11-06T22:00:00.000Z"}
 />, rootElement);
