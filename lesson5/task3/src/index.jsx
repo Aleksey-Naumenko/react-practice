32354c89b0605cd08761bec6ecd2868c0bcfef00
Aleksey <aleksey_naumenko@ukr.net>
2020-02-07 23:21:27 +0200

    3

---
+++ lesson5/task3/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Counter from './Counter';
+
+const rootElem = document.querySelector('#root');
+
+ReactDOM.render(<Counter />, rootElem);
