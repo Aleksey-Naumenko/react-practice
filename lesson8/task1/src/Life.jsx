ff7745d09f391b548b09525d5a6709d8f78dec2f
Aleksey <aleksey_naumenko@ukr.net>
2020-02-11 21:48:02 +0200

    1

---
+++ lesson8/task1/src/Life.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+
+class Life extends Component {
+    constructor(props) {
+        super(props);
+
+        console.log('constructor: good place to create state');
+    }
+
+    componentDidMount() {
+        console.log('componentDidMount: API calls, subscriptions');
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render');
+        return true;
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log('some updates based on new props');
+    }
+
+    componentWillUnmount() {
+        console.log('componentWillUnmount(): cleanup before DOM related to component will be removed');
+    }
+
+    render() {
+        console.log('return React element to build DOM');
+        return (
+            <div>Text Here</div>
+        )
+    }
+}
+
+export default Life;
---
+++ lesson8/task1/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Life from './Life';
+
+
+const rootElem = document.querySelector('#root');
+
+ReactDOM.render(<Life />, rootElem);
