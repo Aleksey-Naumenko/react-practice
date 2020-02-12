4317bc7920b5bfaadf2bb942be0cc903e873d28d
Aleksey <aleksey_naumenko@ukr.net>
2020-02-12 09:28:37 +0200

    3

---
+++ lesson8/task3/src/Dimensions.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+
+
+class Dimensions extends Component {
+    constructor(props) {
+        super(props);
+
+        // const { innerWidth, innerHeight } = window;
+
+        this.state = {
+            width: null,
+            height: null,
+        };
+
+    }
+
+    componentDidMount() {
+        window.addEventListener('resize', this.onResize);
+
+        const { innerWidth, innerHeight } = window;
+        this.setDimensions(innerWidth, innerHeight);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.onResize);
+
+    }
+
+    onResize = e => {
+        const { innerWidth, innerHeight } = e.target;
+        this.setDimensions(innerWidth, innerHeight);
+    }
+
+    setDimensions = (width, height) => {
+        this.setState({
+            width,
+            height,
+        });
+        document.title = `${innerWidth}px - ${innerHeight}px`;
+    }
+
+    render() {
+        return (
+            <div className="dimensions">
+                {`${this.state.width}px - ${this.state.height}px`}
+            </div>
+        )
+    }
+}
+
+export default Dimensions;
---
+++ lesson8/task3/src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+
+import Dimensions from './Dimensions';
+
+
+const rootElem = document.querySelector('#root');
+
+ReactDOM.render(<Dimensions />, rootElem);
