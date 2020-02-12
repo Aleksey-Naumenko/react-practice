7a5d708d2141e849a3a087d54db6ce591e705755
Aleksey <aleksey_naumenko@ukr.net>
2020-02-12 18:00:02 +0200

    2

---
+++ lesson9/task2/src/UserForm.jsx
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+
+
+class UserForm extends Component {
+    state = {
+        name: '',
+        student: '',
+        occupation: '',
+        about: '',
+    }
+
+    handleChange = e => {
+        const { name, value, checked, type } = e.target;
+
+        const val = type === 'checkbox'
+            ? checked
+            : value;
+        this.setState({
+            [name]: val,
+        });
+    }
+    handleSubmit = e => {
+        e.preventDefault();
+    }
+
+    render() {
+        return (
+            <form className="login-form" onSubmit={this.handleSubmit}>
+                <h1 className="form-title">Profile</h1>
+                <div className="form-control">
+                    <label className="form-label" htmlFor="name">Name</label>
+                    <input
+                        className="form-input"
+                        value={this.state.name}
+                        type="text"
+                        id="name" name="name"
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <div className="form-control">
+                    <label className="form-label" htmlFor="student">Student</label>
+                    <input 
+                        className="form-input" 
+                        value={this.state.student} 
+                        type="checkbox" id="student" 
+                        name="student"
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <div className="form-control">
+                    <label className="form-label" id="occupation" htmlFor="occupation">Occupation</label>
+                    <select 
+                        name="occupation" 
+                        value={this.state.occupation} 
+                        className="form-input"
+                        onChange={this.handleChange}
+                    >
+                        <option value="london">London</option>
+                        <option value="new-york">New York</option>
+                        <option value="sidney">Sidney</option>
+                        <option value="berlin">Berlin</option>
+                    </select>
+                </div>
+                <div className="form-control">
+                    <label className="form-label" id="about" htmlFor="about">About</label>
+                    <textarea
+                        name="about" 
+                        value={this.state.about} 
+                        className="form-input"
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <button className="submit-button" type="submit">Submit</button>
+            </form>
+        )
+    }
+}
+
+export default UserForm;
---
+++ lesson9/task2/src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+
+import UserForm from './UserForm';
+
+
+const rootElem = document.querySelector('#root');
+
+ReactDOM.render(<UserForm />, rootElem);
