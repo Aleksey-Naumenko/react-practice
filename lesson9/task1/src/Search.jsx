576447b9f2e147679fe68c20659d7ddc0ebabdac
Aleksey <aleksey_naumenko@ukr.net>
2020-02-12 16:14:21 +0200

    1

--- lesson9/task1/src/Search.jsx
+++ lesson9/task1/src/Search.jsx
@@ -11,15 +11,23 @@ class Search extends Component {
         });
     }
 
-    onSearch = () => {        
+    onSearch = e => {
+        e.preventDefault();
         alert(`Search text: ${this.state.value}`);
     }
 
     render() {
         return (
-            <form className="search">
-                <input type="text" className="search__input" onChange={this.onInputChange} value={this.state.value} />
-                <button className="search__button" onClick={this.onSearch}>Search</button>
+            <form className="search" onSubmit={this.onSearch}>
+                <input
+                    type="text"
+                    className="search__input"
+                    onChange={this.onInputChange}
+                    value={this.state.value}
+                />
+                <button className="search__button" type="submit">
+                    Search
+                </button>
             </form>
         )
     }
