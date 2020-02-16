8e68866c921eb08d7d757892d1509d8f5a2798ff
Aleksey <aleksey_naumenko@ukr.net>
2020-02-16 13:01:32 +0200

    hw1

---
+++ lesson10/hw1/src/index.jsx
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+import Calculator from './Calculator';
+
+ReactDOM.render(<Calculator />, document.querySelector('#root'));
