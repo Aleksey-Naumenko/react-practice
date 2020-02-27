a6d123e44f46f6fbd61baeb711197176415fbba6
Aleksey <aleksey_naumenko@ukr.net>
2020-02-27 12:22:03 +0200

    1

---
+++ lesson13/task1/src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './index.scss';
+import Home from './Home';
+import Products from './Products';
+import Contacts from './Contacts';
+import PageNotFound from './PageNotFound';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+
+const App = () => {
+    return (
+        <div className="page">
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                    <Route path="/products">
+                        <Products />
+                    </Route>
+                    <Route path="/contacts">
+                        <Contacts />
+                    </Route>
+                    <Route path="*">
+                        <PageNotFound />
+                    </Route>
+                </Switch>
+            </BrowserRouter>
+        </div>
+    );
+};
+
+export default App;
