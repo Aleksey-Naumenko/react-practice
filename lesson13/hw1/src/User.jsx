66372316d616b498718369c924cd1622c77579ab
Aleksey <aleksey_naumenko@ukr.net>
2020-02-27 19:18:14 +0200

    hw1

--- lesson13/hw1/src/User.jsx
+++ lesson13/hw1/src/User.jsx
@@ -8,24 +8,16 @@ class User extends Component {
         counter: 0
     }
     componentDidUpdate(prevProps, prevState) {
-        console.log('UPDATE happened');
-        console.log(prevProps);
-        console.log(prevState);
-
-        //this.fetchUserData(); // updates when render
         if (prevProps.match.url !== this.props.match.url) {
             this.fetchUserData(this.props.match.url);
         }
     }
-
-    //   userPath = this.props.match.url
 
     componentDidMount() {
         this.fetchUserData(this.props.match.url);
     }
 
     fetchUserData = (userPath) => {
-        // console.log(this.props.match.url);
         fetch(`https://api.github.com${userPath}`)
             .then(response => response.json())
             .then(user => this.setState({
@@ -44,11 +36,6 @@ class User extends Component {
                     <span className="user__name">{userName}</span>
                     <span className="user__location">{userLocation}</span>
                 </div>
-                <button onClick={() => this.setState({
-                    counter: this.state.counter + 1
-                })}>
-                    Counter now is on {this.state.counter}
-                </button>
             </div>
         );
     }
