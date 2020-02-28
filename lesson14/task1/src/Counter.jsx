2126e287dbfadd3baa5a0d25418eea0198a8d8a1
Aleksey <aleksey_naumenko@ukr.net>
2020-02-28 11:22:37 +0200

    1

---
+++ lesson14/task1/src/Counter.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+
+const Counter = () => {
+    const [ counter, setCounter ] = useState(0);
+
+    return (
+        <div className="counter">
+            <button className="counter__button" onClick={() => setCounter(counter - 1)}>
+                -
+            </button>
+            <span className="counter__value">
+                {counter}
+            </span>
+            <button className="counter__button" onClick={() => setCounter(counter + 1)}>
+                +
+            </button>
+        </div>
+    );
+};
+
+export default Counter;
