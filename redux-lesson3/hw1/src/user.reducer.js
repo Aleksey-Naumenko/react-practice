565b2f2775648e334022a1e354d5a3e90867b999
Aleksey <aleksey_naumenko@ukr.net>
2020-03-04 10:26:32 +0200

    hw1

--- redux-lesson3/hw1/src/user.reducer.js
+++ redux-lesson3/hw1/src/user.reducer.js
@@ -3,13 +3,9 @@ import { ADD, DELETE } from './user.actions';
 export const userReducer = (state = null, action) => {
     switch (action.type) {
         case ADD:
-            return {
-                user: action.payload.userData,
-            };
+            return action.payload.userData;
         case DELETE:
-            return {
-                user: null
-            };
+            return null;
         default:
             return state;
     }
