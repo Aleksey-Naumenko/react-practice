bbcd8a6d314126d9b59e61dc0e9fa5274ce49511
Aleksey <aleksey_naumenko@ukr.net>
2020-03-10 20:37:18 +0200

    2

--- redux-lesson6/task2/src/users/UserInfo.jsx
+++ redux-lesson6/task2/src/users/UserInfo.jsx
@@ -15,7 +15,7 @@ const UserInfo = ({ userData, isFetching }) => {
                 <img alt="User Avatar" src={userData.avatar_url} className="user__avatar" />
                 <div className="user__info">
                     <span className="user__name">{userData.name}</span>
-                    <span className="user__location">{userData.location}</span>
+                    <span className="user__location">{`from ${userData.location}`}</span>
                 </div>
             </div>
         );
