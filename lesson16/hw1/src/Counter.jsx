eb2551d2054fe7449a91fcb82571b763e3eabe0f
Aleksey <aleksey_naumenko@ukr.net>
2020-04-03 22:21:00 +0300

    tests

---
+++ lesson16/hw1/src/Counter.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+    state = {
+        counter: 0,
+    };
+
+    increment = () => {
+        this.setState({
+            counter: this.state.counter + 1,
+        });
+    };
+
+    decrement = () => {
+        this.setState({
+            counter: this.state.counter - 1,
+        });
+    };
+
+    reset = () => {
+        this.setState({
+            counter: 0,
+        });
+    };
+
+    render() {
+        return (
+            <div className="counter">
+                <button className="counter__button" onClick={this.decrement}>
+                    -
+                </button>
+                <span className="counter__value" onClick={this.reset}>
+                    {this.state.counter}
+                </span>
+                <button className="counter__button" onClick={this.increment}>
+                    +
+                </button>
+            </div>
+        );
+    }
+}
+
+export default Counter;
---
+++ lesson16/hw1/src/__tests__/Counter.test.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import Counter from '../Counter';
+import { shallow } from 'enzyme';
+// import { getUserData } from '../usersGateway';
+
+// jest.mock('../usersGateway', () => {
+//   return {
+//     getUserData: jest.fn(() => Promise.resolve())
+//   }
+// });
+
+describe('Counter', () => {
+  it ('initial state should be equal to "0" ', () => {
+    const wrappedComponent = shallow(<Counter />);
+    const initialState = wrappedComponent.state('counter');
+
+    expect(initialState).toEqual(0);
+  });
+
+  it ('should decrease counter by "1" with every click', () => {
+    const wrappedComponent = shallow(<Counter />);
+    const componentState = wrappedComponent.state('counter');
+
+    wrappedComponent.find('.counter__button:first-child').simulate('click');
+    const stateAfterClick = wrappedComponent.state('counter');
+
+    expect(stateAfterClick).toEqual(componentState - 1);
+  });
+
+  it ('should increase counter by "1" with every click', () => {
+    const wrappedComponent = shallow(<Counter />);
+    const componentState = wrappedComponent.state('counter');
+
+    wrappedComponent.find('.counter__button:last-child').simulate('click');
+    const stateAfterClick = wrappedComponent.state('counter');
+
+    expect(stateAfterClick).toEqual(componentState + 1);
+  });
+
+  it ('should nullify counter', () => {
+    const wrappedComponent = shallow(<Counter />);
+
+    wrappedComponent.find('.counter__value').simulate('click');
+    const stateAfterClick = wrappedComponent.state('counter');
+
+    expect(stateAfterClick).toEqual(0);
+  });
+});
